a32bf786504553e304167669e94d57323a9207fb
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2020-12-03 21:35:19 +0800

    Add files via upload

---
+++ loginDAO.java
@@ -0,0 +1,51 @@
+package org.example.dao;
+
+import org.example.exception.AppException;
+import org.example.model.User;
+import org.example.util.DBUtil;
+
+import java.sql.Connection;
+import java.util.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class loginDAO {
+    public static User query(String username) {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+            c = DBUtil.getConnection();
+            String sql = "select id, username, password, " +
+                    "nickname, sex, birthday, head " +
+                    "from user where username=?";
+            ps = c.prepareStatement(sql);
+            //设置占位符
+            ps.setString(1,username);
+            rs = ps.executeQuery();
+            User user = null;
+            while(rs.next()){
+                user = new User();
+                //设置User值
+                user.setId(rs.getInt("id"));
+                user.setUsername(username);
+                user.setPassword(rs.getString("password"));
+                user.setNickname(rs.getString("nickname"));
+                user.setSex(rs.getBoolean("sex"));
+                //关于日期的使用 java中使用 java.util.Date(年月日时分秒)
+                //rs.getDate()返回值是java.sql.Date(时分秒)
+                //rs.getTimestamp()返回java.sql.TimaStamp(年月日时分秒)
+                java.sql.Date birthday = rs.getDate("birthday");
+                if(birthday!=null)
+                    user.setBirthday(new Date(birthday.getTime()));
+                user.setHead(rs.getString("head"));
+            }
+            return user;
+        }catch(Exception e){
+            throw new AppException("LOG001","查询用户操作出错",e);
+        }finally{
+            DBUtil.close(c,ps,rs);
+        }
+    }
+}
