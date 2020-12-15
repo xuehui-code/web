40edcfd39c1d28a92399beba8c694e45c298e7fa
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2020-12-15 16:08:28 +0800

    Add files via upload

---
+++ ThreadLook5.java
@@ -0,0 +1,25 @@
+package lesson1;
+
+public class ThreadLook5 {
+
+    public static void main(String[] args) {
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                for(int i=0; i<10000; i++){
+                    //某个线程抛异常，如果抛到run方法，整个线程直接终止，
+                    // 但是不会影响其他线程
+                    // 线程中，处理异常的方式：线程对象.setUncaughtExceptionHandler
+                    // 或者自己在run()捕获并处理
+                    if(i == 20)
+                        throw new RuntimeException();
+                    System.out.println(i);
+                }
+            }
+        };
+        t.start();
+        while(true){
+
+        }
+    }
+}
